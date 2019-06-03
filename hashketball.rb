ea300222594e117e849f5f132a76defa4875834f
Alyssa Yesilyurt <alyssa.yesilyurt@gmail.com>
2019-06-03 17:29:26 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -165,3 +165,18 @@ def player_numbers(team_name)
   end
   jersey_numbers
 end
+
+
+def player_stats(player_name)
+  game_hash.each do |location, team|
+    team.each do |attribute, data|
+     if attribute == :players
+       data.each do |player, stats|
+        if player == player_name
+          return stats
+        end
+       end
+      end
+    end
+  end
+end
